3e08c4a89c472eaa7fc05f84ab8b3d4cc0ac3e5c
DaniOmehe <danielomehe66@gmail.com>
2023-05-23 17:00:24 +0100

    Fixed a bug in the hotels page

--- pages/hotels.js
+++ pages/hotels.js
@@ -1,12 +1,8 @@
-import { Fragment } from "react"
-import { Button } from "../layout/header/banner/style"
-
 const Hotels = ()=>{
     return(
-        <Fragment>
+        <>
             <h1>This is the Hotels page</h1>
-            <Button>Click</Button>
-        </Fragment>
+        </>
     )
 }
 
