a0661a2e975a000f4dc0a08a6f5271dc382533b9
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-05-24 14:01:48 +0100

    utility Section Title

--- components/utility/button.js
+++
@@ -1,8 +0,0 @@
-import { Button } from "./style"
-const Buttons = ({ children })=>{
-    return(
-        <Button background='#3EB489' color="#fff"> {children} </Button>
-    )
-}
-
-export default Buttons
---
+++ components/utility/section_title/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const sectionWraper = styled.div`
+    display: flex;
+    align-items: center;
+`
+export const Title = styled.h2`
+    font-size: 32px;
+    font-weight: 600;
+    margin-bottom: 4px;
+
+`
+export const Text = styled.p`
+    font-size: 18px;
+`
