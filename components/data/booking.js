af2ef8d7e74fc44e7e002f5bf77559ff71aad3b4
DaniOmehe <danielomehe66@gmail.com>
2023-05-26 14:13:08 +0100

    Build banner section

--- components/data/booking.js
+++ components/data/booking.js
@@ -1,16 +1,18 @@
 export const booking = [
     {
-        image: '/images/flight.png',
+        image: '/images/flight.jpg',
         title: 'Flights',
         subtitle: 'Search Flights & Places Hire to our most popular destinations',
         url: '/flights',
-        text: 'Show Flights'
+        text: 'Show Flights',
+        icon: 'images/plane.svg'
     },
     {
         image: '/images/hotel.png',
         title: 'Hotels',
         subtitle: 'Search hotels & Places Hire to our most popular destinations',
         url: '/hotels',
-        text: 'Show Hotels'
+        text: 'Show Hotels',
+        icon: 'images/hotel.svg'
     },
 ]
