a43513f5a982aa03998c5b897a71fc2b1372f579
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-05-29 18:41:17 +0100

    updated newsletter

--- layout/footer/index.js
+++ layout/footer/index.js
@@ -1,8 +1,14 @@
 import { FooterWrapper, FooterItems } from "./style";
+import Newsletter from "./newsletter";
+import { FooterWrapper, FooterItems, FooterItem, FooterLogo, FooterIcons, FooterIcon, FooterItemHeader, FooterLinks, FooterLink } from "./style";
+import { footerItems } from "../../components/data/navigations/navigations";
+import Link from 'next/link'
 
 const Footer =()=>{
     return(
         <FooterWrapper>
+            <Newsletter/>
+            <FooterItems></FooterItems>
             <FooterItems>
                 <FooterItem height='100px'>
                     <FooterLogo src="images/Logo.png" />
