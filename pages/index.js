6cbf8796c641b577530f917cc79c0c33ccd9930b
kolawole-ysf <111007555+kolawole-ysf@users.noreply.github.com>
2023-05-31 11:34:05 +0100

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Header from "../layout/header";
 import { FlightPlan, Places, Banners, Reviews } from "../components/landing-page";
 import Footer from "../layout/footer";
-import { FlightPlan, Places, Banners, Reviews, } from "../components/landing-page";
 import LandingContext from "../context/landing-page";
 
 const LandingPage = () => {
