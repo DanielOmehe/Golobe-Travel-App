bf59ca869d4e068ca66a4916e8332fd01ea659b4
kolawole-ysf <111007555+kolawole-ysf@users.noreply.github.com>
2023-06-01 11:18:47 +0100

    update

---
+++ components/data/locations.js
@@ -0,0 +1,43 @@
+export const locations = [
+    {
+        city: 'Melbourne',
+        image: '/images/melbourne.svg',
+        text: 'An amazing journey',
+        prize: '$700'
+    },
+    {
+        city: 'Paris',
+        image: '/images/paris.svg',
+        text: 'A Paris adventure',
+        prize: '$600'
+    },
+    {
+        city: 'London',
+        image: '/images/london.svg',
+        text: 'A London eye adventure',
+        prize: '$350'
+    },
+    {
+        city: 'Colombia',
+        image: '/images/colombia.svg',
+        text: 'Amazing streets',
+        prize: '$700'
+    },
+    
+]
+
+export const views = [
+    {
+        image: '/images/sri-lanka1.svg',
+    },
+    {
+        image: '/images/sri-lanka2.svg',
+    },
+    {
+        image: '/images/sri-lanka3.svg',
+    },
+    {
+        image: '/images/sri-lanka4.svg',
+    },
+    
+]
