06f377532cb44f4abb1ed207514ae1734d3a5785
kolawole-ysf <111007555+kolawole-ysf@users.noreply.github.com>
2023-06-01 12:25:53 +0100

    commit

--- pages/flights.js
+++ pages/flights.js
@@ -1,18 +1,11 @@
-import Locations from "../components/flight-page/locations"
-import Places from "../components/flight-page/places"
-import { FlightPlan } from "../components/landing-page"
 import LandingContext from "../context/landing-page"
 import Layout from "../layout"
-import Explore from "../components/flight-page/locations/explore"
 
 const Flights = ()=>{
     return(
         <LandingContext>
             <Layout>
-                <FlightPlan />
-                <Places />
-                <Locations />
-                <Explore />
+              This is flight page
             </Layout>
         </LandingContext>
     )
